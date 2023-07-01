d84a74a282b8feae4a6557c49652666067a24249
I D H S Karunasena <138161428+himashasathsaranie@users.noreply.github.com>
2023-07-02 02:31:11 +0530

    Add files via upload

---
+++ Lab 1/Q-2/Q-1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius of the circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double area = CalculateCircleArea(radius);
+
+            Console.WriteLine("The area of the circle is: " + area);
+
+            Console.ReadLine();
+        }
+        static double CalculateCircleArea(double radius)
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+    }
+}
---
+++ Lab 1/Q-4/Q-1.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the employee's salary: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the tax rate(in decimal form): ");
+            double taxRate = Convert.ToDouble(Console.ReadLine());
+
+            double salaryAfterTax =salary * (1-taxRate);
+
+            Console.WriteLine("Salary after tax: " + salaryAfterTax);
+             
+            Console.ReadLine();
+        }
+    }
+}
