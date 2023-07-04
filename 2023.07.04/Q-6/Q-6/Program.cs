ffe77b0f86c3e8c7ca1c8456a061f12c27cd7ea1
I D H S Karunasena <138161428+himashasathsaranie@users.noreply.github.com>
2023-07-04 22:52:05 +0530

    Add files via upload

---
+++ 2023.07.04/Q-6/Q-6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the size of rhe array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            ArrayClass obj1 = new ArrayClass();
+            int[] array = obj1.arrayMethod(size);
+
+            Console.WriteLine("\nDisplay the array: ");
+            for(int i=0;i<array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
